aab501bc0a1cdaba984d789003010412b072a31a
steeling <teeling.sean@gmail.com>
2014-11-05 21:35:12 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Polyfriends::Application.config.secret_key_base = '680788367acd95fd364f492599b663979729e9661a3f948d8dcbadcd519dfc1777c0307cb9000c8eba3cd087d83e6a2661ee1a0429b6697113d6d77693269f90'
