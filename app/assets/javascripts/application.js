5e14d5c89d42bf7a27d191af772a398f5e3d75a4
Sean Teeling <teeling.sean@gmail.com>
2015-01-09 11:38:07 -0800

    fix stars dev

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,35 +14,38 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require chosen-jquery
-//= require bootstrap-datepicker
 //= require_tree .
 
-$(document).ready(function(){
-  $('.stars-big').raty({
-    readOnly: true,
-    hints   : [null, null, null, null, null],
-    starOn  : 'star-on-big.png',
-    starOff : 'star-off-big.png',
-    starHalf: 'star-half-big.png',
-    score   : function() {
-      return $(this).attr('data-score');
-    }
-  });
 
-  $('.stars').raty({
-    readOnly: true,
-    hints   : [null, null, null, null, null],
-    score   : function() {
-      return $(this).attr('data-score');
-    }
-  });
+  $(document).ready(function(){
+    $('.stars-big').raty({
+      readOnly: true,
+      hints   : [null, null, null, null, null],
+      starOn  : 'star-on-big.png',
+      starOff : 'star-off-big.png',
+      starHalf: 'star-half-big.png',
+      path: '/assets',
+      score   : function() {
+        return $(this).attr('data-score');
+      }
+    });
 
-  $('.star-input').raty({
-    readOnly: false,
-    hints: [null, null, null, null, null],
-    half: true,
-    click: function(score, evt) {
-      $('.star-form-helper').val(score);
-    }
+    $('.stars').raty({
+      readOnly: true,
+      hints   : [null, null, null, null, null],
+      path: '/assets',
+      score   : function() {
+        return $(this).attr('data-score');
+      }
+    });
+
+    $('.star-input').raty({
+      readOnly: false,
+      hints: [null, null, null, null, null],
+      half: true,
+      path: '/assets',
+      click: function(score, evt) {
+        $('.star-form-helper').val(score);
+      }
+    });
   });
-});
